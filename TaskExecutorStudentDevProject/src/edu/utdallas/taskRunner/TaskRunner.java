588d91b779a1b182aec5f28430d1d593af6d5dc4
Ryan C Hood <ryanchristopherhood@gmail.com>
2020-03-17 11:19:01 -0500

    Finished Task Runner Class

--- TaskExecutorStudentDevProject/src/edu/utdallas/taskRunner/TaskRunner.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/taskRunner/TaskRunner.java
@@ -1,20 +1,29 @@
 package edu.utdallas.taskRunner;
 
-import edu.utdallas.taskExecutor.Task;
 import edu.utdallas.blockingFIFO.BlockingFIFO;
+import edu.utdallas.taskExecutor.Task;
 
 
 public class TaskRunner implements Runnable {
+	// Since the threads need access to the fifo (to take tasks), we must include it as an instance variable of class.
+	private BlockingFIFO fifo;
 	
+	// The constructor simply initializes the fifo.
+	public TaskRunner(BlockingFIFO fifo) {
+		this.fifo = fifo;
+	}
+	
+	// The threads will go here to the run() method.  The run method is a simply infinite loop that attempts to 
+	// take a task from the fifo, and then execute that task. If this fails, the system will print an error message.
+	@Override
 	public void run() {
 		while(true) {
-			Task task = fifo.take();
 			try {
+				Task task = fifo.take();
 				task.execute();
 			}
-			catch(Throwable th) {
-				System.out.print("The following task failed to execute: ");
-				System.out.print(task.getName());
+			catch(Exception e) {
+				System.out.print("The system either couldn't retrieve a task from the fifo or it couldn't execute the task.");
 				System.out.println();
 			}
 		}
