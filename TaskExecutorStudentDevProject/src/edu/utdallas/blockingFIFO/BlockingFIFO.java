6bb880e783490f9544da70f8141c49ea5325be2c
Ryan C Hood <ryanchristopherhood@gmail.com>
2020-03-18 11:43:48 -0500

    Updated code to be very close to the solution // There's a problem somewhere in this code though

--- TaskExecutorStudentDevProject/src/edu/utdallas/blockingFIFO/BlockingFIFO.java
+++ TaskExecutorStudentDevProject/src/edu/utdallas/blockingFIFO/BlockingFIFO.java
@@ -3,7 +3,7 @@ package edu.utdallas.blockingFIFO;
 import edu.utdallas.taskExecutor.Task;
 
 public class BlockingFIFO {
-	Task[] buffer = new Task[100];
+	Task buffer[];
 	int nextIn;
 	int nextOut;
 	int count;
@@ -11,32 +11,47 @@ public class BlockingFIFO {
 	Object notEmpty;
 	
 	public BlockingFIFO() {
+		buffer = new Task[100];
 		nextIn = 0;
 		nextOut = 0;
 		count = 0;
+		notFull = new Object();
+		notEmpty = new Object();	
 	}
 	
 	public void put(Task task) throws InterruptedException {
-		if (count == buffer.length) {
-			notFull.wait();
+		while (true) {
+			System.out.print("HI -- put");
+			if (count == 100) {
+				notFull.wait();
+				continue;
+			}
+			else {
+				break;
+			}
 		}
-		
 		synchronized(this) {
 			buffer[nextIn] = task;
-			nextIn = (nextIn + 1) % buffer.length;
+			nextIn = (nextIn + 1) % 100;
 			count++;
 			notEmpty.notify();
 		}
 	}
 	
 	public Task take() throws InterruptedException {
-		if (count == 0) {
-			notEmpty.wait();
+		while (true) {
+			if (count == 0) {
+				System.out.print("HI -- take");
+				notEmpty.wait();
+				continue;
+			}
+			else {
+				break;
+			}
 		}
-		
 		synchronized(this) {
 			Task result = buffer[nextOut];
-			nextOut = (nextOut + 1) % buffer.length;
+			nextOut = (nextOut + 1) % 100;
 			count--;
 			notFull.notify();
 			return result;
